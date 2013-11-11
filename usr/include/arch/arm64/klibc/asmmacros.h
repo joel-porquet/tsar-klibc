b80998b5d5bdacc23bb93d65ed3a8cfc6cb649b8
Steve Capper <steve.capper@linaro.org>
2013-11-11 17:04:12 +0000

    [klibc] arm64: Add arm64 support
    Based on work by Neil Williams (codehelp@debian.org) and Anil Singhar
    (anil.singhar@linaro.org), this patch adds arm64 support.
    
    Originally-by: Neil Williams <codehelp@debian.org>
    Originally-by: Anil Singhar <anil.singhar@linaro.org>
    Signed-off-by: Steve Capper <steve.capper@linaro.org>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

---
+++ usr/include/arch/arm64/klibc/asmmacros.h
@@ -0,0 +1,11 @@
+/*
+ * usr/include/arch/arm64/klibc/asmmacros.h
+ *
+ * Assembly macros used by arm64 system call stubs
+ */
+
+#ifndef _KLIBC_ASMMACROS_H
+#define _KLIBC_ASMMACROS_H
+
+
+#endif /* _KLIBC_ASMMACROS_H */
